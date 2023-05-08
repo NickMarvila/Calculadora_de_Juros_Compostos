6db0402d21702c6508cb2a9fa90913b81454c219
Nicolas Marvila <nickmarvila@gmail.com>
2023-05-08 00:09:59 -0300

    projeto calculadora de juros compostos v1.0

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+  //pegando os elementos pelo ID
+document.getElementById("calculate").addEventListener("click", function () {
+  const value = document.getElementById("value").value;
+  const fee = document.getElementById("fee").value / 100;
+  const time = document.getElementById("time").value;
+
+  //formula dos juros compostos
+  const total = value * (1 + fee) ** time;
+
+  document.getElementById("total").innerHTML =
+    "R$ " + total.toFixed(2).replace(".", ",");
+});
